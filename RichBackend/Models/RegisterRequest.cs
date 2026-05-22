cc4bf8b78537f0dc474c15b44c03517256552edf
Rojda Yıldız <yildizrojda213@gmail.com>
2026-05-22 15:25:43 +0300

    Rojda backend proje konfigurasyonu eklendi

--- RichBackend/Models/RegisterRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace RichBackend.Models
-{
-    public class RegisterRequest
-    {
-        public string FullName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
--- RichBackend/Services/IEventPublisher.cs
+++
@@ -1,7 +0,0 @@
-namespace RichBackend.Services
-{
-    public interface IEventPublisher
-    {
-        void Publish(string eventName, object payload);
-    }
-}
