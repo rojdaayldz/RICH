b5959bd831b27d0f408db84292138718654e6ec1
Buketttt <karacalibuket@gmail.com>
2026-06-03 19:32:06 +0300

    Kategori arama ve stil düzenlemeleri yapıldı

---
+++ web-frontend/src/components/KategoriNotu.md
@@ -0,0 +1,3 @@
+# Kategori ve arama notu
+
+Kategori/renk/beden filtrelerinin UI tarafında daha okunabilir yönetilebilmesi için yardımcı filtre listeleri eklendi.
---
+++ web-frontend/src/config/catalogFilters.js
@@ -0,0 +1,14 @@
+export const defaultColors = [
+  "Lacivert",
+  "Siyah",
+  "Beyaz",
+  "Kahverengi",
+  "Krem",
+  "Bordo",
+  "Gri",
+  "Haki",
+  "Altın",
+  "Gümüş",
+];
+
+export const defaultSizes = ["S", "M", "L", "XL", "Standart", "0-3 Ay", "6-9 Ay", "9-12 Ay"];
